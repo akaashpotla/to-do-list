a2448d1ab17ea2213d4d68b972a1390e3f1d6f40
akaashpotla1 <akaash@tntgamez.com>
2026-02-11 11:55:52 -0600

    Indented lines properly

--- tests/api/users.py
+++ tests/api/users.py
@@ -11,7 +11,7 @@ class TestUserCreation:
             "name" : NAME,
             "email" : EMAIL,
             "password" : PASSWORD
-            }
+        }
         response = client.post(URL, json = payload)
         assert response.status_code == 201
         data = response.json()
@@ -23,105 +23,105 @@ class TestUserCreation:
 
     def test_name_missing(self, client):
         payload = {
-                "email" : EMAIL,
-                "password" : PASSWORD
+            "email" : EMAIL,
+            "password" : PASSWORD
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_name_empty_string(self, client):
         payload = {
-                "name" : "",
-                "email" : EMAIL,
-                "password" : PASSWORD
+            "name" : "",
+            "email" : EMAIL,
+            "password" : PASSWORD
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_name_too_long(self, client):
         payload = {
-                "name" : "a" * 256,
-                "email" : EMAIL,
-                "password" : PASSWORD
+            "name" : "a" * 256,
+            "email" : EMAIL,
+            "password" : PASSWORD
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_email_missing(self, client):
         payload = {
-                "name" : NAME,
-                "password" : PASSWORD
+            "name" : NAME,
+            "password" : PASSWORD
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_email_empty_string(self, client):
         payload = {
-                "name" : NAME,
-                "email" : "",
-                "password" : PASSWORD
+            "name" : NAME,
+            "email" : "",
+            "password" : PASSWORD
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_email_non_email_string(self, client):
         payload = {
-                "name" : NAME,
-                "email" : "akaashpotla123",
-                "password" : PASSWORD
+            "name" : NAME,
+            "email" : "akaashpotla123",
+            "password" : PASSWORD
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_missing_password(self, client):
         payload = {
-                "name" : NAME,
-                "email" : EMAIL
+            "name" : NAME,
+            "email" : EMAIL
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_short_password(self, client):
         payload = {
-                "name" : NAME,
-                "email" : EMAIL,
-                "password" : "Apple1!"
+            "name" : NAME,
+            "email" : EMAIL,
+            "password" : "Apple1!"
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_long_password(self, client):
         payload = {
-                "name" : NAME,
-                "email" : EMAIL,
-                "password" : "1Password!kvjnscknaoisdjcffwkenvwivejnkdsjncjksndkjcvbsidbvjkskdjnvkjsdv"
+            "name" : NAME,
+            "email" : EMAIL,
+            "password" : "1Password!kvjnscknaoisdjcffwkenvwivejnkdsjncjksndkjcvbsidbvjkskdjnvkjsdv"
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_password_without_uppercase(self, client):
         payload = {
-                "name" : NAME,
-                "email" : EMAIL,
-                "password" : "1password!"
+            "name" : NAME,
+            "email" : EMAIL,
+            "password" : "1password!"
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_password_without_special_character(self, client):
         payload = {
-                "name" : NAME,
-                "email" : EMAIL,
-                "password" : "1Password"
+            "name" : NAME,
+            "email" : EMAIL,
+            "password" : "1Password"
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
 
     def test_password_without_number(self, client):
         payload = {
-                "name" : NAME,
-                "email" : EMAIL,
-                "password" : "Password!"
+            "name" : NAME,
+            "email" : EMAIL,
+            "password" : "Password!"
         }
         response = client.post(URL, json = payload)
         assert response.status_code == 422
